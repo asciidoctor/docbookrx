486fdab733f7758554d1ca015b15b8f014094cd0
Kjell Irgens <hextremist@gmail.com>
2015-12-29 11:18:32 +0100

    Add support for qandaset elements
    (Also: No longer inserts unnecessary leading newlines in visit_text)

--- lib/docbookrx/docbook_visitor.rb
+++ lib/docbookrx/docbook_visitor.rb
@@ -905,8 +905,11 @@ class DocbookVisitor
         if is_first
           text = text.lstrip
         elsif leading_space_match && !!(text !~ LeadingSpaceRx)
-          # QUESTION if leading space was an endline, should we restore the endline or just put a space char?
-          text = %(#{leading_space_match[0]}#{text})
+          if @lines[-1] == "----" || @lines[-1] == "===="
+            text = %(#{leading_space_match[0]}#{text})
+          else
+            text = %( #{text})
+          end
         end
 
         # FIXME sentence-per-line logic should be applied at paragraph block level only
@@ -1243,6 +1246,43 @@ class DocbookVisitor
       append_text '\'' * 3
     end
     false
+  end
+
+  def visit_qandaset node
+    node.elements.to_a.each do |quandadiv|
+      quandadiv.elements.each do |element|
+        if element.name == 'title'
+          append_line ".#{element.text}"
+          append_blank_line
+          append_line '[qanda]'
+        elsif element.name == 'qandaentry'
+          id = resolve_id element, normalize: @normalize_ids
+          if (question = element.at_xpath 'db:question/db:para', 'db': DocbookNs)
+            append_line %([[#{id}]]) if id
+            append_line (format_text question) + "::"
+            if (answer = element.at_xpath 'db:answer', 'db': DocbookNs)
+              first = true
+              answer.children.each_with_index do |child, i|
+                unless child.text.rstrip.empty?
+                  unless first
+                    append_line '+'
+                    @continuation = true
+                  end
+                  first = nil
+                  child.accept self
+                end
+              end
+              @continuation = false
+            else
+              warn %(Missing answer in quandaset!)
+            end
+            append_blank_line
+          else
+            warn %(Missing question in quandaset! Skipping.)
+          end
+        end
+      end
+    end
   end
 
   def lazy_quote text, seek = ','
