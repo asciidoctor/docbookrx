f608398ad0a97b481fc31e52c3a31431ba6e2d22
Dan Allen <dan.j.allen@gmail.com>
2023-10-12 02:32:59 -0600

    clean out unused cruft from rspec scaffolding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'bundler/setup'
 require 'docbookrx'
-Dir[File.join(File.dirname(__FILE__), 'support/extensions/**/*.rb')].each { |file| require file }
-Dir[File.join(File.dirname(__FILE__), 'support/kit/**/*.rb')].each { |file| require file }
+
+Dir[(File.join (File.dirname __FILE__), 'support/**/*.rb')].each {|file| require file }
